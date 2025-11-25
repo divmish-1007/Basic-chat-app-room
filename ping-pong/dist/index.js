f2720f4885e81255874807cbd2bb35aca9ca6a5a
divmish-1007 <divakarmishra5301@gmail.com>
2025-11-25 22:27:44 +0530

    Basic ping-pong app

---
+++ ping-pong/dist/index.js
@@ -0,0 +1,10 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8050 });
+wss.on("connection", function (socket) {
+    console.log("User Connected");
+    socket.on("message", (e) => {
+        if (e.toString() === "ping") {
+            socket.send("pong");
+        }
+    });
+});
